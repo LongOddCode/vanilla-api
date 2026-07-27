1d307fc94a31142f0241580011fd00980d7d0f88
Long Hao <haolong@microsoft.com>
2026-07-27 07:48:53 +0000

    07/27 v5

--- api/message/index.js
+++ api/message/index.js
@@ -7,6 +7,6 @@
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
   context.res = {
-    body: { text: "Hello from the API 7/27 v4" },
+    body: { text: "Hello from the API 7/27 v5" },
   };
 };
